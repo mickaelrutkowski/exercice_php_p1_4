891548ea7d015d158491879115a353f06f83eaa8
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-15 14:37:18 +0100

    php exo4

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+<?php
+$age = 17;
+$poids = 57.32;
+$variable = "Mon \" nom \" est Mateo";
+$zero = true;
+$php = false;
+ settype($age, "integer");
+ settype($poids, "float");
+ settype($variable, "string");
+ settype($zero, "string");
+  settype($php, "bool"); 
+echo $variable . " " . $age . " " . $poids . " " . $zero . " " .$php
+
+?>
+</body>
+</html>
